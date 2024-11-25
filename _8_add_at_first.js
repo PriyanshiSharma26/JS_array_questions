250955a38d3f6fadc0f3e4ed03c7cbfdfef6cee3
PriyanshiSharma26 <priya612683@gmail.com>
2024-11-25 10:54:36 -0800

    Add files via upload

---
+++ _8_add_at_first.js
@@ -0,0 +1,14 @@
+
+//8. Add an element to the beginning of the array [8, 9, 10]. 
+
+let arr=[8,9,10]
+let firstelement=7
+
+for(let i=arr.length-1;i>=0;i--)
+{
+    arr[i+1]=arr[i]
+}
+arr[0]=firstelement
+console.log(arr)
+
+
