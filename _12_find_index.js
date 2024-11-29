bfe7f555616a7558204d5142ec2b498bd2a430a8
PriyanshiSharma26 <priya612683@gmail.com>
2024-11-29 20:46:15 +0530

    Add files via upload

---
+++ _12_find_index.js
@@ -0,0 +1,13 @@
+// find index of 20 element in an array
+
+
+let arr=[10,15,20,50,60,40,65]
+
+for(let i=0;i<arr.length;i++)
+{
+    if(arr[i]===15)
+    {
+        console.log("index of 15 is",i)
+        break;
+    }
+}
