d42a3dea8c9933e684b8e7846b5c472c30865a0c
PriyanshiSharma26 <priya612683@gmail.com>
2024-12-01 13:50:35 +0530

    Add files via upload

---
+++ _16_sort_in_desc.js
@@ -0,0 +1,19 @@
+// sort  array in descending order 
+
+let arr=[1,2,3,4,5]
+
+for(let i=0;i<arr.length;i++)
+{
+    for(let j=i+1;j<arr.length;j++)
+    {
+        if(arr[i]<arr[j])
+        {
+            let temp=arr[i]
+            arr[i]=arr[j]
+            arr[j]=temp
+        }
+    }
+    
+}
+
+console.log(arr)
