15a21990e221467e694abb229ce3972e27b421a6
PriyanshiSharma26 <priya612683@gmail.com>
2024-12-02 20:05:04 +0530

    Add files via upload

---
+++ _19_sum.js
@@ -0,0 +1,11 @@
+// Calculate the sum of all elements in the array [2, 4, 6, 8]. 
+
+let arr=[2, 4, 6, 8];
+
+let sum=0
+
+for(let i=0;i<arr.length;i++)
+{
+    sum=sum+arr[i]
+}
+console.log("sum of all element is :",sum)
