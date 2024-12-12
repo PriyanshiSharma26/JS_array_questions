3daa085cc0c72a98fceeb8028997c72017e1ec6c
PriyanshiSharma26 <priya612683@gmail.com>
2024-12-12 21:09:47 +0530

    Add files via upload

--- _31_rotate_array.js
+++ _31_rotate_array.js
@@ -1,1 +1,18 @@
-// Rotate the array [1, 2, 3, 4, 5] to the left by 2 positions. 
+// Rotate the array [1, 2, 3, 4, 5] to the left by 2 positions.
+
+
+let arr=[1, 2, 3, 4, 5]
+let arr2=[]
+let i=0;
+
+let temp=arr[0]
+for(let j=1;j<arr.length;j++)
+{
+    arr2[i]=arr[j]
+    i++
+
+}
+arr2[arr2.length]=temp
+
+
+console.log(arr2)
